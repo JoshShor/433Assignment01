0ce8514e7b3d4b2eb123a0bedfd6286567fa0153
TamN81 <78515664+TamN81@users.noreply.github.com>
2022-09-19 21:48:53 -0700

    v2

--- pcb.h
+++ pcb.h
@@ -1,7 +1,7 @@
 /**
  * Assignment 1: priority queue of processes
  * @file pcb.h
- * @author Joshua Shor, Tam Nguyen
+ * @author ??? (TODO: your name)
  * @brief This is the header file for the PCB class, a process control block.
  * @version 0.1
  */
@@ -82,7 +82,7 @@ public:
      * @param state
      */
     void setState(ProcState state) {
-        this -> state = state;  // TODO: add your code here
+         this -> state = state; 
     }
 
     /**
@@ -90,7 +90,7 @@ public:
      * @param priority
      */
     void setPriority(unsigned int priority) {
-        this-> priority = priority; // TODO: add your code here
+          this-> priority = priority;
     }
 
     /**
