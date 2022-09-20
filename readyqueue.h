96c0cab8eb5683eac891d63cf2c0ba132dd95a64
TamN81 <78515664+TamN81@users.noreply.github.com>
2022-09-19 22:41:34 -0700

    v4

--- readyqueue.h
+++ readyqueue.h
@@ -6,19 +6,17 @@
  * It should be a priority queue such that the process with the highest priority can be selected next.
  * @version 0.1
  */
-//You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
-// Remember to add sufficient comments to your code
-#pragma once
 
+#pragma once
 #include "pcb.h"
 
 /**
  * @brief A queue of PCB's that are in the READY state to be scheduled to run.
  * It should be a priority queue such that the process with the highest priority can be selected next.
  */
-class ReadyQueue {
+class ReadyQueue
+{
 private:
-    // TODO: add your private member variables here
     int arr_size;
     int capacity;
     PCB *arr;
@@ -36,42 +34,63 @@ public:
      */
     ~ReadyQueue() {}
 
-	// You may add additional member functions, but don't change the definitions of the following four member functions.
+    // You may add additional member functions, but don't change the definitions of the following four member functions.
 
     /**
      * @brief Add a PCB representing a process into the ready queue.
      *
      * @param pcbPtr: the pointer to the PCB to be added
      */
-	void addPCB(PCB* pcbPtr);
+    void addPCB(PCB *pcbPtr);
 
     /**
      * @brief Remove and return the PCB with the highest priority from the queue
      *
      * @return PCB*: the pointer to the PCB with the highest priority
      */
-	PCB* removePCB();
+    PCB *removePCB();
 
     /**
      * @brief Returns the number of elements in the queue.
      *
      * @return int: the number of PCBs in the queue
      */
-	int size();
+    int size();
 
-     /**
-      * @brief Display the PCBs in the queue.
-      */
-	void displayAll();
-
+    /**
+     * @brief Display the PCBs in the queue.
+     */
+    void displayAll();
+    /**
+     * @brief support queue
+     */
     void siftDown(int i);
-
+    /**
+     * @brief support queue
+     */
     void siftUp(int i);
+    /**
+     * @brief support queue
+     */
     PCB getMax();
+    /**
+     * @brief support queue
+     */
     bool isLeaf(int i);
+    /**
+     * @brief support queue
+     */
     int right(int i);
-     int left(int i);
+    /**
+     * @brief support queue
+     */
+    int left(int i);
+    /**
+     * @brief support queue
+     */
     int parent(int i);
+    /**
+     * @brief support queue
+     */
     void swap(PCB arr[], int q, int i);
-
 };
